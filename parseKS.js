8fd8aa4eb7bdd54be3c36ea5f6490c754740fc98
Ronin Protheroe <rprotheroe@protonmail.com>
2020-06-03 20:02:20 -0500

    Added Include tag

--- parseKS.js
+++ parseKS.js
@@ -1,48 +1,71 @@
+var fs = require('fs')
+
 function parseKS(html) {
     let vars = {}
 
-    var vr = new RegExp(/<(variable) ([A-z]\w+)>([\S\s]+?)<\/variable>/g);
-    if (html.split(vr)[1] !== undefined) {
-        var arrayed = html.split(vr)
+    var include = new RegExp(/<(include)>([.-z]+)<\/include>/g);
+    if (html.split(include)[1] !== undefined) {
+        var arrayed = html.split(include)
         var i;
         for (i = 1; i < arrayed.length; i++) {
-            if(arrayed[i] === "variable"){
+            if (arrayed[i] === 'include') {
+                try {
+                    data = fs.readFileSync(arrayed[i+1])
+                    arrayed[i] = ''
+                    arrayed[i+1] = data
+                    html = arrayed.join(' ')
+                    return html
+                } catch (error) {
+                    arrayed[i] = ''
+                    arrayed[i+1] = error
+                    html = arrayed.join(' ')
+                    return html
+                }
+            }
+        }
+    }
+
+    var timestamp = new RegExp(/<timestamp\/>()/g);
+    if (html.split(timestamp)[1] !== undefined) {
+        var arrayed = html.split(timestamp)
+        var i;
+        for (i = 1; i < arrayed.length; i++) {
+            if (arrayed[i] == '') {
+                arrayed[i] = Date.now()
+                html = arrayed.join(' ')
+            }
+        }
+    }
+
+    var variable = new RegExp(/<(variable) ([A-z]\w+)>([\S\s]+?)<\/variable>/g);
+    if (html.split(variable)[1] !== undefined) {
+        var arrayed = html.split(variable)
+        var i;
+        for (i = 1; i < arrayed.length; i++) {
+            if (arrayed[i] === "variable") {
                 vars[arrayed[i + 1]] = arrayed[i + 2]
                 console.log(vars)
                 arrayed[i] = '';
-                arrayed[i + 1] = ''; 
+                arrayed[i + 1] = '';
                 arrayed[i + 2] = '';
                 html = arrayed.join(' ')
             }
         }
     }
 
-    var eo = new RegExp(/<(echo)>(\w+)<\/echo>/g);
-    if (html.split(eo)[1] !== undefined) {
-        var arrayed = html.split(eo)
+    var echo = new RegExp(/<(echo)>(\w+)<\/echo>/g);
+    if (html.split(echo)[1] !== undefined) {
+        var arrayed = html.split(echo)
         console.log(arrayed)
         var i;
         for (i = 1; i < arrayed.length; i++) {
-            if(arrayed[i] === 'echo'){
+            if (arrayed[i] === 'echo') {
                 arrayed[i] = ''
-                arrayed[i+1] = vars[arrayed[i+1]]
+                arrayed[i + 1] = vars[arrayed[i + 1]]
                 html = arrayed.join(' ')
             }
         }
     }
-
-    var ts = new RegExp(/<timestamp\/>()/g);
-        if (html.split(ts)[1] !== undefined) {
-            var arrayed = html.split(ts)
-            var i;
-            for (i = 1; i < arrayed.length; i++) {
-                if(arrayed[i] == ''){
-                    arrayed[i] = Date.now()
-                    html = arrayed.join(' ')
-                }
-            }
-        }
-
     return html
 }
 
